133c026f4988fbed215abf55cde47fffae5798fd
Francisco Leví <francisco.levi.md@gmail.com>
2022-01-12 21:27:27 -0600

    Post tweet logic.

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ class Tweet(BaseModel):
         min_length=1
     )
     created_at: datetime = Field(deafult=datetime.now())
-    update_at: Optional[datetime] = Field(deafult=None)
+    updated_at: Optional[datetime] = Field(deafult=None)
     by: User = Field(...)
 
 
@@ -214,8 +214,38 @@ def show_all_tweets():
     summary="Post a tweet",
     tags=["Tweets"]
 )
-def post():
-    pass
+def post(tweet: Tweet = Body(...)):
+    """
+    Post a Tweet
+
+    This path operation post a tweet in the app.
+
+    Parameters:
+    - Request body parameter
+        - tweet: Tweet
+
+    Returns a json with the basic tweet information:
+    - tweet_id: UUID
+    - content: str
+    - created_at: datetime
+    - updated_at: Optional[datetime]
+    - by: User
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        if tweet_dict["updated_at"]:
+            tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        if tweet_dict["by"]:
+            tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+            tweet_dict["by"]["birth_date"] = str(
+                tweet_dict["by"]["birth_date"])
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
 
 
 # ### Show a tweet
